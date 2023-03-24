5836b6e63fe7841ebfb8efae1eda277cf6b9c1d3
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-03-24 12:28:42 -0700

    Data-source options for production

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -1,15 +1,27 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USERNAME || 'coffee_app',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_DATABASE || 'coffee_db',
-  schema: process.env.DB_SCHEMA || 'public',
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/db/migrations/*{.ts,.js}'],
-};
+export const dataSourceOptions: DataSourceOptions = (() => {
+  if (process.env.DATABASE_URL) {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      schema: process.env.DB_SCHEMA || 'public',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/db/migrations/*{.ts,.js}'],
+    } as DataSourceOptions;
+  } else {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      username: process.env.DB_USERNAME || 'coffee_app',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_DATABASE || 'coffee_db',
+      schema: process.env.DB_SCHEMA || 'public',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/db/migrations/*{.ts,.js}'],
+    } as DataSourceOptions;
+  }
+})();
 
 const dataSource = new DataSource(dataSourceOptions);
 
