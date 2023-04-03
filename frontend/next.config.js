d8a56632ec0034028c0b320a096d163717d8bdd4
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-04-03 09:28:18 -0700

    Render.yaml

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: "/api/:path*",
-                destination: "http://localhost:5000/:path*",
+                destination: "http://localhost:8080/:path*",
             },
         ];
     },
